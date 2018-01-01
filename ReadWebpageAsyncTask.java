8908baead66983141abb1d9d3ddc4e430f9655d8
Parthuin <blalonde28@gmail.com>
2018-01-01 18:17:20 -0500

    Add files via upload
    Async Task Activity

---
+++ ReadWebpageAsyncTask.java
@@ -0,0 +1,56 @@
+package com.example.blalonde9489.projectapp;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by Brandon Lalonde on 1/1/2018.
+ */
+
+public class ReadWebpageAsyncTask extends Activity {
+    private TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.t);
+    }
+
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            // we use the OkHttp library from https://github.com/square/okhttp
+            OkHttpClient client = new OkHttpClient();
+            Request request =
+                    new Request.Builder()
+                            .url(urls[0])
+                            .build();
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+
+            return "Download failed";
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        textView.setText(result);
+    }
+}
+
+    // Triggered via a button in your layout
+    public void onClick(View view) {
+        DownloadWebPageTask task = new DownloadWebPageTask();
+        task.execute(new String[] { "http://www.vogella.com/index.html" });
+
+    }
+}
