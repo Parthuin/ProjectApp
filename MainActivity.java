a193c7ca679443145d1a897b76e01aa95f42d82e
Parthuin <blalonde28@gmail.com>
2018-01-01 21:13:19 -0500

    Async working

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.blalonde9489.projectapp;
 
 import android.app.IntentService;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
@@ -11,10 +16,12 @@ import android.support.annotation.RequiresPermission;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,7 +30,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private static int TIME_OUT = 4000;
     SharedPreferences my_preferences;
-
+    ImageView image;
+    String URL = "https://vignette.wikia.nocookie.net/leonhartimvu/images/d/d3/001_Bulbasaur_DW.png/revision/latest?cb=20131106174201";
+    ProgressDialog mProgressDialog;
     private User user;
     private AppDatabase database;
     private ListView listview;
@@ -34,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.my_preferences = PreferenceManager
                 .getDefaultSharedPreferences(this);
         database = AppDatabase.getDatabase(getApplicationContext());
+        image=(ImageView)findViewById(R.id.imgView2);
         listview=(ListView) findViewById(R.id.listview);
         // cleanup for testing some initial data
         // add some data
@@ -126,8 +136,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         if(view.getId()==R.id.btnpoke)
         {
-            ReadWebpageAsyncTask myTask = new ReadWebpageAsyncTask();
-            myTask.onClick(view);
+            new DownloadImage().execute(URL);
         }
         // TODO call updatefirstUserData
 
@@ -155,5 +164,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return true;
         }
 
+    }
+    private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Create a progressdialog
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            // Set progressdialog title
+            mProgressDialog.setTitle("Download Image Tutorial");
+            // Set progressdialog message
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            // Show progressdialog
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... URL) {
+
+            String imageURL = URL[0];
+
+            Bitmap bitmap = null;
+            try {
+                // Download Image from URL
+                InputStream input = new java.net.URL(imageURL).openStream();
+                // Decode Bitmap
+                bitmap = BitmapFactory.decodeStream(input);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            // Set the bitmap into ImageView
+            image.setImageBitmap(result);
+            // Close progressdialog
+            mProgressDialog.dismiss();
+        }
     }
 }
